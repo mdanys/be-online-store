bf76c6212b1f796920eb791d6d39f24085b666e3
mdanys <mochammaddany@gmail.com>
2023-07-02 23:24:05 +0700

    adding skeleton

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `user` (
+    `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
+    `email`                 VARCHAR(255)    NOT NULL UNIQUE,
+    `password`              VARCHAR(255)    NOT NULL,
+    `name`                  VARCHAR(255)    NOT NULL,
+    `role`                  VARCHAR(255)    NOT NULL,
+    `dob`                   DATE,           NOT NULL,
+    `gender`                VARCHAR(1),     NOT NULL,
+    `address`               TEXT,
+    `picture`               VARCHAR(1024),
+    `dtm_crt`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `dtm_upd`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `category` (
+    `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
+    `name`                  VARCHAR(255)    NOT NULL,
+    `dtm_crt`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `dtm_upd`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `product` (
+    `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
+    `category_id`           BIGINT          NOT NULL,
+    `name`                  VARCHAR(255)    NOT NULL,
+    `price`                 DECIMAL(10,2)   NOT NULL,
+    `qty`                   BIGINT          NOT NULL,
+    `rating`                DECIMAL(2,1),
+    `detail`                TEXT,
+    `picture`               VARCHAR(1024),
+    `dtm_crt`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `dtm_upd`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `cart` (
+    `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
+    `user_id`               BIGINT          NOT NULL,
+    `product_id`            BIGINT          NOT NULL,
+    `dtm_upd`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `order` (
+    `id`                    BIGINT          NOT NULL AUTO_INCREMENT,
+    `cart_id`               BIGINT          NOT NULL,
+    `total_price`           DECIMAL(10,2)   NOT NULL,
+    `dtm_crt`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `dtm_upd`               TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
