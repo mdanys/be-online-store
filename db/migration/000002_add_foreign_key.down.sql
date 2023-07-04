8a1b9b91adf81f80b9ccd77b17c6fabe1ad18abc
mdanys <mochammaddany@gmail.com>
2023-07-05 01:26:27 +0700

    adding fk user id in order

--- db/migration/000002_add_foreign_key.down.sql
+++ db/migration/000002_add_foreign_key.down.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `product` DROP FOREIGN KEY `fk_course_category_id`;
 ALTER TABLE `cart`    DROP FOREIGN KEY `fk_cart_user_id`;
 ALTER TABLE `cart`    DROP FOREIGN KEY `fk_cart_product_id`;
+ALTER TABLE `order`   DROP FOREIGN KEY `fk_order_user_id`;
 ALTER TABLE `order`   DROP FOREIGN KEY `fk_order_cart_id`;
